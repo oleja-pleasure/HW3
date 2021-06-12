7d1a92931d21196da97a794effc1ef53942742f3
oleja_pleasure <oleg-paleev1@mail.ru>
2021-06-12 11:50:12 +0300

    Исправил названия классов

--- src/test/java/FindJUnit5Example.java
+++ src/test/java/FindJUnit5Example.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class findJUnit5Example {
+public class FindJUnit5Example {
 
     @BeforeAll
     static void setUpConfig() {
